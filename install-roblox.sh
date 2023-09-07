83da41d78833cb444d9e964678ec0101a8d9f7ca
Lava <69821200+RealLava@users.noreply.github.com>
2023-09-07 01:50:59 -0400

    Create install-roblox.sh

---
+++ install-roblox.sh
@@ -0,0 +1,3 @@
+flatpak install flathub io.github.vinegarhq.Vinegar
+mkdir -p ~/.var/app/io.github.vinegarhq.Vinegar/config/vinegar/ && { echo "[player]"; echo "dxvk = false"; echo "renderer = \"Vulkan\""; } >> ~/.var/app/io.github.vinegarhq.Vinegar/config/vinegar/config.toml
+flatpak run io.github.vinegarhq.Vinegar player -app
