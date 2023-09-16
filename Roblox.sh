987c7bfd2843c319d581052b9634b26b611610e6
Lava <69821200+RealLava@users.noreply.github.com>
2023-09-16 17:34:32 -0400

    Update Roblox.sh

--- Roblox.sh
+++ Roblox.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 flatpak install flathub io.github.vinegarhq.Vinegar
 mkdir -p ~/.var/app/io.github.vinegarhq.Vinegar/config/vinegar/ && { echo "[player]"; echo "dxvk = false"; echo "renderer = \"Vulkan\""; } >> ~/.var/app/io.github.vinegarhq.Vinegar/config/vinegar/config.toml
 flatpak run io.github.vinegarhq.Vinegar player -app
